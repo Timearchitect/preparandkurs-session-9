4b16db9e6691b297621917b4361932932f68f87d
Alrik He <alrik_he@hotmail.com>
2024-11-06 10:34:02 +0100

    first

---
+++ tillfälle 1 2024-11-04 kväll/script/script.js
@@ -0,0 +1,32 @@
+
+/*
+alert(123)
+document.write(456)
+console.log(789)
+*/
+
+
+// 1) variabler (behållare)
+
+let alrik  // skapar/deklarerar en variabel
+alrik = 5  // lägger in/tilldela ett värde
+alrik = 10 
+alrik = 5 + 10
+
+alrik = (2 + 2) * 3   // kalkylation
+
+alrik = "hej" + "san" // konkatination
+
+alrik = "hej" + (1 + 1)  //hej11
+
+alrik =  1 + 1 + "hej" //2hej 
+
+//P arentess
+//E xponent
+//M ultiplikation
+//D ivition
+//A ddition
+//S ubtraktion
+let medina = 2 //deklarering + tilldelning
+
+console.log( medina + alrik )
---
+++ tillfälle 1 2024-11-04/script/script.js
@@ -0,0 +1,27 @@
+
+alert( 123 )
+document.write(" <h1> titel </h1> ")
+console.log( 789 )
+
+
+document.title = "ALRIK"
+//document.getElementsByTagName("h1")[0].style.backgroundColor="blue"
+
+// 1) variabler
+let x  //deklarera en variabel som heter x   
+x = 5  //tilldelar variabeln x med siffran 5
+x = "hello world" //tilldelar variabeln x med String hello world
+console.log(12345678)
+
+let z = 10 //skapa en varaiabel lägg in 10
+z = "ALRIK är cool!!!!!"
+console.log(z)
+console.log(5+5)
+console.log("hej"+"san");
+
+x = 5;
+let y = 5;
+z = x + y + 5;
+console.log(z);
+
+
