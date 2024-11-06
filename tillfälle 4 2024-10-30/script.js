ca17a7e5c1b60338efb4c76e97dfdd268371580c
Alrik He <alrik_he@hotmail.com>
2024-11-06 10:37:54 +0100

    second

---
+++ tillfälle 4 2024-10-30/script.js
@@ -0,0 +1,25 @@
+
+//array
+
+let array = ["hej",1,true]
+array[6] = "ALRIK"
+array.push(true,"hejsan",77)
+
+// object
+//   key:värde
+let skor1 = {  pris:1000 , namn:"Nike2000" ,storlek:40 }
+
+//array är Object egentligen i javascript
+skor1.pris = 500 
+skor1["pris"] = 250 
+
+console.log(array,skor1);
+document.write(`<h1>HELLO WORLD</h1> 
+    <hr> <hr>`)
+
+
+function alrik(x) { 
+    return 5 + 1
+}
+
+Math.random() 
